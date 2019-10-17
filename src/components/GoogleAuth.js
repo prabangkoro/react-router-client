c7659f51554b36e0b2b17db6d9d0dbeae1f160c5
Bagus Prabangkoro (babang) <bagus.prabangkoro@gdn-commerce.com>
2019-10-17 16:04:07 +0700

    add google service

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class GoogleAuth extends Component {
+  componentDidMount () {
+    // window.gapi.load('auth2', function() {
+    //   window.gapi.auth2.init({
+    //     clientId: '498898731549-1vlgblf2374ehhrskopn0gnmmtli6eic.apps.googleusercontent.com',
+    //     scope: 'email'
+    //   })
+    // })
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
